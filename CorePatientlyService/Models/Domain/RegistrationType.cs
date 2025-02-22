a97481dbde97e195c21054724bbb5793558ec947
natnaelh14 <natnaelh14@github.com>
2025-02-21 22:52:22 -0500

    created domain models

---
+++ CorePatientlyService/Models/Domain/RegistrationType.cs
@@ -0,0 +1,8 @@
+namespace CorePatientlyService.Models.Domain;
+
+public enum RegistrationType
+{
+    SIGNUP,
+    START_USER_REGISTRATION,
+    COMPLETE_USER_REGISTRATION
+}
---
+++ CorePatientlyService/Models/Domain/Tenant.cs
@@ -0,0 +1,18 @@
+namespace CorePatientlyService.Models.Domain;
+
+public class Tenant
+{
+    private Guid id;
+
+    private String name;
+
+    private String streetAddress;
+
+    private String city;
+
+    private String country;
+
+    private String zipcode;
+
+    private String pictureUrl;
+}
