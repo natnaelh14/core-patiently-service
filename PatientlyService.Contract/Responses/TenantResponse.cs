ad3c998e0f63dd816b98cccf8a63461e8ec26285
natnaelh14 <natnaelh14@github.com>
2025-03-27 13:31:16 -0400

    fix: create tenant endpoint'

--- PatientlyService.Contract/Responses/TenantResponse.cs
+++ PatientlyService.Contract/Responses/TenantResponse.cs
@@ -4,17 +4,10 @@ public class TenantResponse
 {
     public required Guid Id { get; init; }
     public required string Name { get; init; }
-
     public required string StreetAddress { get; init; }
-    
     public required string City { get; init; }
-    
     public required string State { get; init; }
-    
     public required string Country { get; init; }
-    
     public required string ZipCode { get; init; }
-    
     public required string PictureUrl { get; init; }
-
 }
