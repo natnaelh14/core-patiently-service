b45a49505d873c772fe523e7d880f5b40a4cdb26
natnaelh14 <natnaelh14@github.com>
2025-05-26 22:00:55 -0400

    change dot net version

--- PatientlyService.API/obj/Debug/net8.0/PatientlyService.API.AssemblyInfo.cs
+++ PatientlyService.API/obj/Debug/net8.0/PatientlyService.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatientlyService.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef651e5c69b41f3a2e50b89fedd76ebfff2920b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a738416a86bce7b8ce95488f8a41fc5d7ec5c008")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatientlyService.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatientlyService.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
